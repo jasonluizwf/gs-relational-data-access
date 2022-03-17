02dfbac24722ff0d01108ecd4ec188e9570aa288
Victor <victorluizfacul@gmail.com>
2022-03-16 22:40:15 -0300

    Initial commit

---
+++ src/main/java/com/example/gsrelationaldataaccess/Customer.java
@@ -0,0 +1,23 @@
+package com.example.gsrelationaldataaccess;
+
+public class Customer {
+    private long id;
+    private String firstName, lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /*  @Overrider é uma anotação que informa ao compilador que a intenção seria de sobrescrever.
+        Dessa forma, o compilador pode avaliar se a assinatura está coerente com algum método das
+        superclasses e emite um aviso caso o método não esteja realmente sobrescrevendo algo!
+     */
+    @Override
+    public String toString() {
+        return String.format(
+                "[id='%d', firstName='%s', lastName='%s']",
+        id, firstName, lastName);
+    }
+}
