68f60d9de688f921b4b2cbe70015d035f5ca7002
Victor <victorluizfacul@gmail.com>
2022-03-16 23:28:55 -0300

    Criação da do jdbcTemplate e comentarios no codigo

--- src/main/java/com/example/gsrelationaldataaccess/Customer.java
+++ src/main/java/com/example/gsrelationaldataaccess/Customer.java
@@ -1,5 +1,7 @@
 package com.example.gsrelationaldataaccess;
 
+
+//A lógica de acesso a dados simples com a qual você trabalhará gerencia o nome e o sobrenome dos clientes.
 public class Customer {
     private long id;
     private String firstName, lastName;
@@ -9,7 +11,6 @@ public class Customer {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
     /*  @Overrider é uma anotação que informa ao compilador que a intenção seria de sobrescrever.
         Dessa forma, o compilador pode avaliar se a assinatura está coerente com algum método das
         superclasses e emite um aviso caso o método não esteja realmente sobrescrevendo algo!
--- src/main/java/com/example/gsrelationaldataaccess/RelationalDataAccessApplication.java
+++ src/main/java/com/example/gsrelationaldataaccess/RelationalDataAccessApplication.java
@@ -1,13 +1,71 @@
 package com.example.gsrelationaldataaccess;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+@SpringBootApplication é uma anotação de conveniência que adiciona todos os itens a seguir:
+
+@Configuration: marca a classe como uma fonte de definições de bean para o contexto do aplicativo.
+
+@EnableAutoConfiguration: diz ao Spring Boot para começar a adicionar beans,
+com base nas configurações do caminho de classe, outros beans e várias configurações de propriedade.
+
+@ComponentScan: diz ao Spring para procurar outros componentes, configurações e serviços no
+com.example.relationaldataaccesspacote. Neste caso, não há.
+ */
 
 @SpringBootApplication
-public class RelationalDataAccessApplication {
+public class RelationalDataAccessApplication implements CommandLineRunner {
+
+	private static final Logger log =
+			LoggerFactory.getLogger(RelationalDataAccessApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RelationalDataAccessApplication.class, args);
 	}
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		log.info("Creating tables");
+
+		jdbcTemplate.execute("DROP TABLE customer IF EXISTS");
+		jdbcTemplate.execute("CREATE TABLE customer(" +
+				"id SERIAL, firs_name VARCHAR(255)," +
+				"last_name VARCHAR(255))");
+
+		//Dividindo a matriz de nomes inteiros em uma matriz de nomes/sobrenomes
+		List<Object[]> splitUpNames = Arrays.asList("John Woo",
+				"Jeff Dean", "Josh Long").stream()
+				.map(name -> name.split(" "))
+				.collect(Collectors.toList());
+
+		//Usando um fluxo Java 8 para imprimir cada tupla da lista
+		splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s"
+				, name[0], name[1])));
+
+		//Usando a operação batchUpdate do JdbcTemplate para carregar dados em massa
+		jdbcTemplate.batchUpdate(
+				"INSERT INTO customers(first_name," +
+						" last_name) VALUES (?,?)", splitUpNames);
+
+		log.info("Querying for customer records where first_name = 'Josh':");
+		jdbcTemplate.query(
+				"SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
+				(rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+		).forEach(customer -> log.info(customer.toString()));
+	}
 }
